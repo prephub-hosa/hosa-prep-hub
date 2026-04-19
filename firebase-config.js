fc4dcf39364b7dfb783f1b6a41a81d86ae0e49d4
HOSA Prep Hub <hosa@prephub.com>
2026-04-19 10:45:53 -0700

    Initial deploy — HOSA Prep Hub with Firebase auth and mobile fixes

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClZOIzxfmGNW5CbqcvR5XNIWcrd8HHsuI",
+  authDomain: "hosa-prep-hub.firebaseapp.com",
+  databaseURL: "https://hosa-prep-hub-default-rtdb.firebaseio.com",
+  projectId: "hosa-prep-hub",
+  storageBucket: "hosa-prep-hub.firebasestorage.app",
+  messagingSenderId: "63423053435",
+  appId: "1:63423053435:web:2cd0127ab4b5837b2e6cde",
+  measurementId: "G-7FSJSJ30XL"
+};
+
+firebase.initializeApp(firebaseConfig);
